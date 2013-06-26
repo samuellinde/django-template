d8bae951f12245e31ff5209e54def570a6a5666b
Samuel Linde <samuel@dontblink.se>
2013-06-26 08:37:30 +0200

    Set up settings module to support different environments.

--- project_template/project_template/settings/base.py
+++ project_template/project_template/settings/base.py
@@ -1,25 +1,8 @@
-# Django settings for project_template project.
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -29,11 +12,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Stockholm'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'sv-se'
 
 SITE_ID = 1
 
@@ -83,7 +66,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'w46l!v)5!u9qt4zus85kzv3hs=!&q!!ze5n!is+n()u&@xbckd'
+SECRET_KEY = 'ty7rlk4g!-v@xi%&m*#rj*r4^$l7t(chw_-&z(riqb6^t02#qs'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
